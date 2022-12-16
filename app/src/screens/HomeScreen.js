d40a5f69158e77dd1dbe86c9d720c244bc8f26a2
Neel <neelnarayanshetty@gmail.com>
2022-12-16 19:20:23 +0530

    app screen setup

--- app/src/screens/HomeScreen.js
+++ app/src/screens/HomeScreen.js
@@ -1,10 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Header from "../components/HomeScreenComponents/Header";
+import Search from "../components/HomeScreenComponents/Search";
+import ChatList from "../components/HomeScreenComponents/ChatList";
 
 const HomeScreen = () => {
   return (
     <View style={styles.root}>
-      <Text>HomeScreen</Text>
+      <View>
+        <Header />
+      </View>
+      <View>
+        <Search />
+      </View>
+      <View>
+        <ChatList />
+      </View>
     </View>
   );
 };
