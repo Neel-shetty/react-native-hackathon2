331ef473b92b44a32a2ccc7aa15a625ed6725d5d
Neel <neelnarayanshetty@gmail.com>
2022-12-17 17:07:46 +0530

    connect client to chat

--- app/App.js
+++ app/App.js
@@ -1,11 +1,14 @@
 import "react-native-gesture-handler";
 import Navigator from "./src/navigator/Navigator";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { OverlayProvider } from "stream-chat-expo";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Navigator />
+      <OverlayProvider>
+        <Navigator />
+      </OverlayProvider>
     </GestureHandlerRootView>
   );
 }
--- app/src/context/chatContext.js
+++ app/src/context/chatContext.js
@@ -1,10 +1,40 @@
+import { useEffect } from "react";
 import { createContext, useContext } from "react";
+import { StreamChat } from "stream-chat";
+import { STREAM_ACCESS_KEY } from "@env";
+import { Auth } from "aws-amplify";
+import { useState } from "react";
 
 const ChatContext = createContext({});
 
 const ChatContextProvider = ({ children }) => {
+  const [chatClient, setChatClient] = useState();
+  useEffect(() => {
+    const initChat = async () => {
+      const client = StreamChat.getInstance(STREAM_ACCESS_KEY);
 
-  
+      const { username } = await Auth.currentUserInfo();
+      await client.connectUser(
+        {
+          id: username,
+          name: "neel",
+          image: "https://randomuser.me/api/portraits/med/women/79.jpg",
+        },
+        client.devToken(username)
+      );
+      setChatClient(client);
+    };
+    initChat();
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (chatClient) {
+        chatClient.disconnectUser();
+      }
+    };
+  }, []);
+
   const value = { username: "cyka blyat" };
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
 };
